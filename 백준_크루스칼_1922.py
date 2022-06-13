252c367842fe334aef8041829230a947d2ddc54e
louis5103 <louis38625103@gmail.com>
2022-06-13 18:40:42 +0900

    백준 크루스칼, Union_Find, 분할문제

--- 백준_크루스칼_1922.py
+++ 백준_크루스칼_1922.py
@@ -1,15 +1,9 @@
 import sys
 
-# node_num = int(sys.stdin.readline())  # N
-# edge_num = int(sys.stdin.readline())  # M
-node_num, edge_num = map(int, sys.stdin.readline().split())
-edge = [list(map(int, sys.stdin.readline().split())) for _ in range(edge_num)]  # G
-
-
 def kruscal(N, M, G):
     G.sort(key=lambda x: x[2])
-    parent = [i for i in range(node_num + 1)]
-    edge_cost = []
+    parent = [i for i in range(node_num)]
+    edge_cost = 0
 
     def parent_find(parent, vertex):
         if parent[vertex] != vertex:
@@ -27,8 +21,17 @@ def kruscal(N, M, G):
 
     for i in range(M):
         if parent_find(parent, G[i][0]) != parent_find(parent, G[i][1]):
-            edge_cost.append(G[i][2])
             union_find(parent, G[i][0], G[i][1])
-    return edge_cost[:-1]
+        else:
+            edge_cost += G[i][2]
+    return edge_cost
 
-print(sum(kruscal(node_num, edge_num, edge)))
+
+while True:
+    node_num, edge_num = map(int, sys.stdin.readline().split())
+    if node_num == 0 and edge_num == 0:
+        break
+
+    edge = [tuple(map(int, sys.stdin.readline().split())) for _ in range(edge_num)]  # G
+    print(kruscal(node_num, edge_num, edge))
+
