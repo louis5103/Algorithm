7fb9665664d3b355d22caf7ef9882a467d1eb68f
louis5103 <louis38625103@gmail.com>
2025-01-14 22:44:52 +0900

    BinaraySearch_1300

---
+++ java/algorithms/src/search/binarySearch/BinarySearch_1300.java
@@ -0,0 +1,14 @@
+package search.binarySearch;
+
+import java.util.Scanner;
+
+public class BinarySearch_1300 {
+    static int N, K;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        
+    }
+}
